be2d02cf33a498dae7624f51a8c14e2cd443bfbd
fqhrmp <fqhrmp@gmail.com>
2021-09-20 09:49:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,29 @@ def create_app():
     def default():
         return render_template("index.html")
 
-
     @app.route('/predict', methods=['POST'])
     def predict():
-        if request.method == 'POST':
-            data_input = request.get_json()["data"]
+	    if request.method == 'POST':
+            data_input = request.get_json()
             data = {}
+
+            data["person_age"] = int(data_input['data']['person_age'])
+            data["person_income"] = int(data_input['data']['person_income'])
+            data["person_home_ownership"] = data_input['data']['person_home_ownership']
+            data["person_emp_length"] = float(data_input['data']['person_emp_length'])
+            data["loan_intent"] = data_input['data']['loan_intent']
+            data["loan_grade"] = data_input['data']['loan_grade']
+            data["loan_amnt"] = int(data_input['data']['loan_amnt'])
+            data["loan_int_rate"] = float(data_input['data']['loan_int_rate'])
+            data["loan_percent_income"] = float(data_input['data']['loan_percent_income'])
+            data["cb_person_default_on_file"] = data_input['data']['cb_person_default_on_file']
+            data["cb_person_cred_hist_length"] = int(data_input['data']['cb_person_cred_hist_length'])
+
+#     @app.route('/predict', methods=['POST'])
+#     def predict():
+#         if request.method == 'POST':
+#             data_input = request.get_json()["data"]
+#             data = {}
 
 #             data["Age"] = data_input.get("Age")
 #             data["Sex"] = data_input.get("Sex")
@@ -30,18 +47,17 @@ def create_app():
 #             data["Duration"] = data_input.get("duration")
 #             data["Purpose"] = data_input.get("purpose")
 
-              data["person_age"] = data_input.get("person_age")
-              data["person_income"] = data_input.get("person_income")
-              data["person_home_ownership"] = data_input.get("person_home_ownership")
-              data["person_emp_length"] = data_input.get("person_emp_length")
-              data["loan_intent"] = data_input.get("loan_intent")
-              data["loan_grade"] = data_input.get("loan_grade")
-              data["loan_amnt"] = data_input.get("loan_amnt")
-              data["loan_int_rate"] = data_input.get("loan_int_rate")
-              data["loan_percent_income"] = data_input.get("loan_percent_income")
-              data["cb_person_default_on_file"] = data_input.get("cb_person_default_on_file")
-              data["cb_person_cred_hist_length"] = data_input.get("cb_person_cred_hist_length")
-
+#               data["person_age"] = data_input.get("person_age")
+#               data["person_income"] = data_input.get("person_income")
+#               data["person_home_ownership"] = data_input.get("person_home_ownership")
+#               data["person_emp_length"] = data_input.get("person_emp_length")
+#               data["loan_intent"] = data_input.get("loan_intent")
+#               data["loan_grade"] = data_input.get("loan_grade")
+#               data["loan_amnt"] = data_input.get("loan_amnt")
+#               data["loan_int_rate"] = data_input.get("loan_int_rate")
+#               data["loan_percent_income"] = data_input.get("loan_percent_income")
+#               data["cb_person_default_on_file"] = data_input.get("cb_person_default_on_file")
+#               data["cb_person_cred_hist_length"] = data_input.get("cb_person_cred_hist_length")
 
             result = make_predictions(data)
             
