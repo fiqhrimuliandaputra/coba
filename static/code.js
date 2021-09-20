268c6313923617efcf4ad03d76a3f50914cd9ffd
fqhrmp <fqhrmp@gmail.com>
2021-09-20 09:08:02 +0700

    Update code.js

--- static/code.js
+++ static/code.js
@@ -2,29 +2,42 @@ var Age_ ,Sex_, Job_, Housing_, saving_account_, checking_account_, credit_amoun
 
 $(document).ready(function(){
   // fetch all DOM elements for the input
-  Age_ = document.getElementById("Age");
-  Sex_ = document.getElementById("Sex");
-  Job_ = document.getElementById("Job");
-  Housing_ = document.getElementById("Housing");
-  saving_account_ = document.getElementById("saving_account");
-  checking_account_ = document.getElementById("checking_account");
-  credit_amount_ = document.getElementById("credit_amount");
-  duration_ = document.getElementById("duration");
-  purpose_ = document.getElementById("purpose");
+  person_age_ = document.getElementById("person_age");
+  person_income_ = document.getElementById("person_income");
+  person_home_ownership_ = document.getElementById("person_home_ownership");
+  person_emp_length_ = document.getElementById("person_emp_length");
+  loan_intent_ = document.getElementById("loan_intent");
+  loan_grade_ = document.getElementById("loan_grade");
+  loan_amnt_ = document.getElementById("loan_amnt");
+  loan_int_rate_ = document.getElementById("loan_int_rate");
+  loan_percent_income_ = document.getElementById("loan_percent_income");
+  cb_person_default_on_file_ = document.getElementById("cb_person_default_on_file");
+  cb_person_cred_hist_length_ = document.getElementById("cb_person_cred_hist_length");
+//   Age_ = document.getElementById("Age");
+//   Sex_ = document.getElementById("Sex");
+//   Job_ = document.getElementById("Job");
+//   Housing_ = document.getElementById("Housing");
+//   saving_account_ = document.getElementById("saving_account");
+//   checking_account_ = document.getElementById("checking_account");
+//   credit_amount_ = document.getElementById("credit_amount");
+//   duration_ = document.getElementById("duration");
+//   purpose_ = document.getElementById("purpose");
 })
 
 $(document).on('click','.button',function(e){
     // on clicking submit fetch values from DOM elements and use them to make request to our flask API
-    var Age = Age_.value;
-    var Sex = Sex_.value;
-    var Job = Job_.value;
-    var Housing = Housing_.value;
-    var saving_account = saving_account_.value;
-    var checking_account = checking_account_.value;
-    var credit_amount = credit_amount_.value;
-    var duration = duration_.value;
-    var purpose = purpose_.value;
-    if(Age == ""){
+    var person_age = person_age_.value;
+    var person_income = person_income_.value;
+    var person_home_ownership = person_home_ownership_.value;
+    var person_emp_length = person_emp_length_.value;
+    var loan_intent = loan_intent_.value;
+    var loan_grade = loan_grade_.value;
+    var loan_amnt = loan_amnt_.value;
+    var loan_int_rate = loan_int_rate_.value;
+    var loan_percent_income= loan_percent_income_.value;
+    var cb_person_default_on_file= cb_person_default_on_file_.value;
+    var cb_person_cred_hist_length= cb_person_cred_hist_length_.value;
+    if(person_age == ""){
       // you may allow it as per your model needs
       // you may mark some fields with * (star) and make sure they aren't empty here
       alert("empty fields not allowed");
@@ -46,16 +59,28 @@ $(document).on('click','.button',function(e){
       // });
       
       var data = {
-          "Age": Age,
-          "Sex": Sex, 
-          "Job": Job, 
-          "Housing": Housing, 
-          "saving_account": saving_account, 
-          "checking_account": checking_account, 
-          "credit_amount": credit_amount, 
-          "duration": duration, 
-          "purpose": purpose
+        "person_age": person_age,
+        "person_income": person_income,
+        "person_home_ownership": person_home_ownership,
+        "person_emp_length": person_emp_length,
+        "loan_intent": loan_intent,
+        "loan_grade": loan_grade,
+        "loan_amnt": loan_amnt,
+        "loan_int_rate": loan_int_rate,
+        "loan_percent_income": loan_percent_income,
+        "cb_person_default_on_file": cb_person_default_on_file,
+        "cb_person_cred_hist_length": cb_person_cred_hist_length
       };
+//           "Age": Age,
+//           "Sex": Sex, 
+//           "Job": Job, 
+//           "Housing": Housing, 
+//           "saving_account": saving_account, 
+//           "checking_account": checking_account, 
+//           "credit_amount": credit_amount, 
+//           "duration": duration, 
+//           "purpose": purpose
+//       };
 
       // # https://api.jquery.com/jquery.post/
       // # https://stackoverflow.com/questions/56032972/sending-a-dictionary-from-js-to-flask-via-ajax
